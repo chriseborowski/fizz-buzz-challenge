54c5db7c422773458c5bbe97e77ee5155b30ffde
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-20 13:03:23 -0500

    Add FizzBuzz description in top comment

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,12 @@
+/*
+FizzBuzz coding challenge
+
+Prompt:
+Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+(source: https://blog.codinghorror.com/why-cant-programmers-program/)
+
+*/
+
 function fizzBuzz() {
   for (let i = 1; i < 101; i++) {
     if (i % 15 === 0) {
